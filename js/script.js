e5d0e03a242150e1462162e47e978a21a890550b
Railan Santos <78385661+railansantos@users.noreply.github.com>
2021-03-07 00:14:04 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,86 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+let direction = "right"; //define direção de movimento da cobrinha
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+
+//Cria Background
+function criarBG() {
+    context.fillStyle = "lightgreen"; //dar cor
+    context.fillRect(0, 0, 16 * box, 16 * box); //dar limites
+}
+
+function criarCobrinha() {
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+
+document.addEventListener('keydown', update); //pega o clique da tecla e chama a função
+
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+
+function iniciarJogo() {
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0; //se passar da tela, volta pelo outro lado
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(iniciarJogo);
+            alert('Game Over :(');
+        }
+    }
+
+    criarBG();
+    criarCobrinha();
+    drawFood();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
+    let newHEad = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHEad);
+}
+
+let jogo = setInterval(iniciarJogo, 100); //passa um intervalo de 100mls para reiniciar o jogo
+
